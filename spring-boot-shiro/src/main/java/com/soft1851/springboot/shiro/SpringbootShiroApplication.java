16a2c1b8e8e7a97b7d26f711dc1becb1f249e872
zhent1106 <1299088269@qq.com>
2020-05-19 12:30:53 +0800

    ✨spring-boot-shiro模块搭建

---
+++ spring-boot-shiro/src/main/java/com/soft1851/springboot/shiro/SpringbootShiroApplication.java
@@ -0,0 +1,13 @@
+package com.soft1851.springboot.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroApplication.class, args);
+    }
+
+}
---
+++ spring-boot-shiro/src/test/java/com/soft1851/springboot/shiro/SpringbootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft1851.springboot.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
