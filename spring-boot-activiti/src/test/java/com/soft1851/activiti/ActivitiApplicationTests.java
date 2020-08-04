d3214f81a6ebdd900bbb8eed103e3cae9daf8222
zhent1106 <1299088269@qq.com>
2020-08-04 23:58:43 +0800

    ✨spring-boot-activiti工作流

---
+++ spring-boot-activiti/src/test/java/com/soft1851/activiti/ActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft1851.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
